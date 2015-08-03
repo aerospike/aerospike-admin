67b4c6bfcfeffad18285000b7dd4dbf92dae2b7b
Pavan <pvn.gupta.2009@gmail.com>
2015-08-03 18:13:00 +0530

    AER-3587 made cluster dun all command version aware

--- lib/node.py
+++ lib/node.py
@@ -19,6 +19,7 @@ from telnetlib import Telnet
 from time import time
 import socket
 import threading
+from distutils.version import StrictVersion
 
 def getfqdn(address, timeout=0.5):
     # note: cannot use timeout lib because signal must be run from the
@@ -453,9 +454,17 @@ class Node(object):
         c = lib.cluster.Cluster(self.ip)
         lookup = c.node_lookup
         result = set()
-
+        
         if 'all' in dun_list:
-            result.add('all')
+            _serviceIP, _servicePort = self._serviceIPPort.split(':')
+            as_version = citrusleaf.citrusleaf_info(_serviceIP, _servicePort)['build']
+            # Comparing with this version because the ability 
+            # to specify "all" in cluster dun was added in 3.3.26
+            if StrictVersion(as_version) < StrictVersion("3.3.26"):
+                # Fetching node id's
+                result = {node.node_id for node in  c.nodes.values()}
+            else:
+                result.add('all')
         else:
             for node in dun_list:
                 if node in lookup:
