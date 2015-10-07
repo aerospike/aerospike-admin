46ce7a68b3a3afef1e2d0f8230dd0dfd94d95dc3
Pavan Gupta <pvn.gupta.2009@gmail.com>
2015-10-07 12:30:30 +0530

    [AER-4329] added watch command test

--- test/e2e/test_collectinfo.py
+++ test/e2e/test_collectinfo.py
@@ -3,13 +3,30 @@ Created on 18-Sep-2015
 
 @author: gslab
 '''
-import unittest
+import test_util
+import unittest2 as unittest
+import lib.util as util
+import lib.controller as controller
 
 
 class TestCollectinfo(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        TestCollectinfo.rc = controller.RootController()
+        actual_out = util.capture_stdout(TestCollectinfo.rc.execute, ['collectinfo'])
 
     def test_collectinfo(self):
+        expected = ['aerospike.log',
+                    'aerospike.conf',
+                    'collectSys.log',
+                    'awsData.log',
+                    'sysInformation/dmesg.log',
+                    'sysInformation/cpu_stat.log',
+                    'sysInformation/sysCmdOutput.log',
+                    'asadmCmd.log',
+                    'clusterCmd.log'
+                    ]
         pass
 
 
