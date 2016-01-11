4375df502dc9f91ef44d4b8790803f1fb28e8791
hbpatre <aerospike@Hemant-MacBook-Pro.local>
2016-01-11 19:28:21 +0530

    Updated license year to 2016

--- test/e2e/test_util.py
+++ test/e2e/test_util.py
@@ -1,8 +1,17 @@
-'''
-Created on 13-Sep-2015
+# Copyright 2013-2016 Aerospike, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-@author: Pavan Gupta
-'''
 import re
 
 def parse_output(actual_out = "", horizontal = False, mearge_header = True):
