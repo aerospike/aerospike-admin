435e3854a8fae207d38d563b5722f792bfe6db22
jboone100 <jboone@aerospike.com>
2018-04-10 10:50:49 -0700

    Modify makefile to build a pex executable

--- create_app.sh
+++ create_app.sh
@@ -7,7 +7,7 @@
 pip wheel -w . .
 
 # Create the pex executable using the asadm wheel and the dependencies from requirements.txt
-pex -v -f . --disable-cache requests asadm -r requirements.txt -c asadm.py -o asadm.pex
+pex -v -f . --disable-cache asadm -r requirements.txt -c asadm.py -o asadm.pex
 
 # Wheel no longer needed, remove it
 rm *.whl
