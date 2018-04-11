420aa787c198a3dbb4f25daa810fdae2075c803a
jboone100 <jboone@aerospike.com>
2018-04-10 17:19:09 -0700

    lib package was causing conflict on ubuntu.

--- create_app.sh
+++ create_app.sh
@@ -4,10 +4,10 @@
 #
 
 # Create the asadm wheel
-pip wheel -w . . -r requirements.txt
+pip wheel -w . .
 
 # Create the pex executable using the asadm wheel and the dependencies from requirements.txt
-pex -v -f . --disable-cache asadm jsonschema lib pexpect pyasn1 pyOpenSSL python_bcrypt mock ply toml unittest2 yappi -c asadm.py -o asadm.pex
+pex -v -f . -r requirements.txt --disable-cache asadm -c asadm.py -o asadm.pex
 
 # Wheels no longer needed, remove them
 rm *.whl
