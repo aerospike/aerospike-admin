b52418ccfc3252f44c415ab922119622fa191a0d
hbpatre <hemant@aerospike.com>
2018-12-04 16:49:34 +0530

    Version updated to 0.1.24.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup (
-	version = "0.1.23",
+	version = "0.1.24",
 	name = "asadm",
 	packages=find_packages(exclude=['doc', 'test*']),
 	include_package_data=True,
