cfc4a5c05129a2b2740440cb1680073a0a5a44f8
Jesse Schmidt <jschmidt@aerospike.com>
2021-02-16 16:50:24 -0800

    Update asadm version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup (
-	version = "2.0.0",
+	version = "2.1.0",
 	name = "asadm",
 	packages=find_packages(exclude=['doc', 'test*']),
 	include_package_data=True,
