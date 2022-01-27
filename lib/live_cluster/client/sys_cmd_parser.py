e2329243292fcb87e4c9711d74684d4e2329b52c
Jesse Schmidt <jschmidt@aerospike.com>
2022-01-27 10:00:03 -0800

    more reorganization

--- lib/live_cluster/client/sys_cmd_parser.py
+++ lib/live_cluster/client/sys_cmd_parser.py
@@ -4,9 +4,9 @@ import math
 import copy
 import logging
 from datetime import datetime
+import sys
 
 from . import section_filter_list
-from . import util
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.CRITICAL)
@@ -117,7 +117,7 @@ def parse_sys_section(section_list, imap, parsed_map):
     for section in section_list:
         if section in parsed_map:
             param_map = {section: parsed_map[section]}
-            util.type_check_basic_values(param_map)
+            type_check_basic_values(param_map)
             parsed_map[section] = copy.deepcopy(param_map[section])
 
 
@@ -213,11 +213,11 @@ def _parse_top_section_line(line, tok_separater, keyval_separater_list):
 
 def _parse_top_section(imap, parsed_map):
     sec_id = "ID_36"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     topdata = {
@@ -373,11 +373,11 @@ def _parse_top_section(imap, parsed_map):
 
 def _parse_uname_section(imap, parsed_map):
     sec_id = "ID_24"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     unamedata = {}
@@ -398,11 +398,11 @@ def _parse_uname_section(imap, parsed_map):
 # output: {key: val..........}
 def _parse_meminfo_section(imap, parsed_map):
     sec_id = "ID_92"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     meminfodata = {}
@@ -436,14 +436,10 @@ def _get_age_month(y, m):
 
 def _parse_lsb_release_section(imap, parsed_map):
     sec_id_1 = "ID_25"
-    raw_section_name_1, final_section_name_1, _ = util.get_section_name_from_id(
-        sec_id_1
-    )
+    raw_section_name_1, final_section_name_1, _ = get_section_name_from_id(sec_id_1)
 
     sec_id_2 = "ID_26"
-    raw_section_name_2, final_section_name_2, _ = util.get_section_name_from_id(
-        sec_id_2
-    )
+    raw_section_name_2, final_section_name_2, _ = get_section_name_from_id(sec_id_2)
 
     logger.info("Parsing section: " + final_section_name_1)
     if not imap:
@@ -528,11 +524,11 @@ def _parse_lsb_release_section(imap, parsed_map):
 
 def _parse_hostname_section(imap, parsed_map):
     sec_id = "ID_22"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     hnamedata = {}
@@ -557,11 +553,11 @@ def _parse_hostname_section(imap, parsed_map):
 
 def _parse_df_section(imap, parsed_map):
     sec_id = "ID_38"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     df_data = []
@@ -623,11 +619,11 @@ def _parse_df_section(imap, parsed_map):
 
 def _parse_free_m_section(imap, parsed_map):
     sec_id = "ID_37"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     free_m_data = {}
@@ -716,17 +712,17 @@ def _parse_free_m_section(imap, parsed_map):
 
 def _modify_keys_in_iostat_section(iostatobj_list):
     for obj in iostatobj_list:
-        util.change_key_name_in_map(obj, ["rkB/s"], "rk_b/s")
-        util.change_key_name_in_map(obj, ["wkB/s"], "wk_b/s")
+        change_key_name_in_map(obj, ["rkB/s"], "rk_b/s")
+        change_key_name_in_map(obj, ["wkB/s"], "wk_b/s")
 
 
 def _parse_dmesg_section(imap, parsed_map):
     sec_id = "ID_42"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     dmesg_section = imap[raw_section_name][0]
@@ -753,11 +749,11 @@ def _parse_dmesg_section(imap, parsed_map):
 
 def _parse_lscpu_section(imap, parsed_map):
     sec_id = "ID_107"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     lscpu_section = imap[raw_section_name][0]
@@ -775,11 +771,11 @@ def _parse_lscpu_section(imap, parsed_map):
 
 def _parse_iptables_section(imap, parsed_map):
     sec_id = "ID_108"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     iptables_section = imap[raw_section_name][0]
@@ -796,11 +792,11 @@ def _parse_iptables_section(imap, parsed_map):
 
 def _parse_sysctlall_section(imap, parsed_map):
     sec_id = "ID_109"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     sysctlall_section = imap[raw_section_name][0]
@@ -818,11 +814,11 @@ def _parse_sysctlall_section(imap, parsed_map):
 
 def _parse_hdparm_section(imap, parsed_map):
     sec_id = "ID_110"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     device_info = {}
@@ -863,11 +859,11 @@ def _parse_hdparm_section(imap, parsed_map):
 
 def _parse_limits_section(imap, parsed_map):
     sec_id = "ID_111"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     limits = {}
@@ -888,11 +884,11 @@ def _parse_limits_section(imap, parsed_map):
 
 def _parse_environment_section(imap, parsed_map):
     sec_id = "ID_112"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     env_section = imap[raw_section_name][0]
@@ -912,11 +908,11 @@ def _parse_environment_section(imap, parsed_map):
 
 def _parse_scheduler_section(imap, parsed_map):
     sec_id = "ID_100"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     scheduler_section = imap[raw_section_name][0]
@@ -977,11 +973,11 @@ def _parse_scheduler_section(imap, parsed_map):
 
 def _parse_iostat_section(imap, parsed_map):
     sec_id = "ID_43"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     iostat_section = imap[raw_section_name][0]
@@ -1106,11 +1102,11 @@ def _parse_iostat_section(imap, parsed_map):
 
 def _parse_interrupts_section(imap, parsed_map):
     sec_id = "ID_93"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     irq_section = imap[raw_section_name][0]
@@ -1147,11 +1143,11 @@ def _parse_interrupts_section(imap, parsed_map):
 
 def _parse_ipaddr_section(imap, parsed_map):
     sec_id = "ID_72"
-    raw_section_name, final_section_name, _ = util.get_section_name_from_id(sec_id)
+    raw_section_name, final_section_name, _ = get_section_name_from_id(sec_id)
 
     logger.info("Parsing section: " + final_section_name)
 
-    if not util.is_valid_section(imap, raw_section_name, final_section_name):
+    if not is_valid_section(imap, raw_section_name, final_section_name):
         return
 
     ip_section = imap[raw_section_name][0]
@@ -1177,14 +1173,10 @@ def _parse_ipaddr_section(imap, parsed_map):
 
 def _parse_awsdata_section(imap, parsed_map):
     sec_id_1 = "ID_70"
-    raw_section_name_1, final_section_name_1, _ = util.get_section_name_from_id(
-        sec_id_1
-    )
+    raw_section_name_1, final_section_name_1, _ = get_section_name_from_id(sec_id_1)
 
     sec_id_2 = "ID_85"
-    raw_section_name_2, final_section_name_2, _ = util.get_section_name_from_id(
-        sec_id_2
-    )
+    raw_section_name_2, final_section_name_2, _ = get_section_name_from_id(sec_id_2)
 
     logger.info("Parsing section: " + final_section_name_1)
     if not imap:
@@ -1242,3 +1234,228 @@ def _parse_awsdata_section(imap, parsed_map):
                 field_count += 1
             continue
     parsed_map[final_section_name_1] = awsdata
+
+
+##########
+# Utils
+##########
+
+# Assumption - Always a valid number is passed to convert to integer/float
+def change_key_name_in_map(datamap, old_keys, new_key):
+    for key in old_keys:
+        if key in datamap:
+            datamap[new_key] = datamap[key]
+            datamap.pop(key, None)
+
+
+def type_check_raw_all(nodes, section_name, parsed_map):
+    for node in nodes:
+        if section_name in parsed_map[node]:
+            _type_check_field_and_raw_values(parsed_map[node][section_name])
+
+
+# This should check only raw values.
+# Aerospike doesn't send float values
+# pretty print and other cpu stats can send float
+# This will skip list if its first item is not a dict.
+
+
+def type_check_basic_values(section):
+    malformedkeys = []
+    # ip_regex = "[0-9]{1,2,3}(\.[0-9]{1,2,3})*"
+    for key in section:
+        if isinstance(section[key], dict):
+            type_check_basic_values(section[key])
+
+        elif (
+            isinstance(section[key], list)
+            and len(section[key]) > 0
+            and isinstance(section[key][0], dict)
+        ):
+            for item in section[key]:
+                type_check_basic_values(item)
+
+        else:
+            if "." in key or " " in key:
+                malformedkeys.append(key)
+            if (
+                isinstance(section[key], list)
+                or isinstance(section[key], int)
+                or isinstance(section[key], bool)
+                or isinstance(section[key], float)
+            ):
+                continue
+            elif section[key] is None:
+                logger.debug("Value for key " + key + " is Null")
+                continue
+            elif section[key] == "N/E" or section[key] == "n/e":
+                logger.debug("'N/E' for the field.")
+                section[key] = None
+                continue
+
+            # Handle float of format (a.b), only 1 dot would be there.
+            if section[key].replace(".", "", 1).isdigit():
+                section[key] = _str_to_number(section[key])
+
+            # Handle bool
+            elif is_bool(section[key]):
+                section[key] = _str_to_boolean(section[key])
+
+            # Handle negative format (-ab,c,f)
+            elif section[key].lstrip("-").isdigit():
+                num = section[key].lstrip("-")
+                if num.isdigit():
+                    number = _str_to_number(num)
+                    section[key] = -1 * number
+
+    for key in malformedkeys:
+        newkey = key.replace(".", "_").replace(" ", "_")
+        val = section[key]
+        section.pop(key, None)
+        section[newkey] = val
+
+
+def get_section_name_from_id(sec_id):
+    raw_section_name = FILTER_LIST[sec_id]["raw_section_name"]
+    final_section_name = (
+        FILTER_LIST[sec_id]["final_section_name"]
+        if "final_section_name" in FILTER_LIST[sec_id]
+        else ""
+    )
+    parent_section_name = (
+        FILTER_LIST[sec_id]["parent_section_name"]
+        if "parent_section_name" in FILTER_LIST[sec_id]
+        else ""
+    )
+    return raw_section_name, final_section_name, parent_section_name
+
+
+def is_collision_allowed_for_section(sec_id):
+    if "collision_allowed" not in FILTER_LIST[sec_id]:
+        return False
+
+    if FILTER_LIST[sec_id]["collision_allowed"] == True:
+        return True
+
+    return False
+
+
+def is_valid_section(
+    imap, raw_section_name, final_section_name, collision_allowed=False
+):
+    if not imap:
+        logger.warning("Null section json")
+        return False
+
+    if raw_section_name not in imap:
+        logger.warning(raw_section_name + " section not present.")
+        return False
+
+    if len(imap[raw_section_name]) > 1 and not collision_allowed:
+        logger.warning(
+            "More than one entries detected, There is a collision for this section: "
+            + final_section_name
+        )
+        return False
+    return True
+
+
+def is_bool(val):
+    return val.lower() in ["true", "false", "yes", "no"]
+
+
+def _str_to_number(number):
+    try:
+        return int(number)
+    except ValueError:
+        try:
+            return float(number)
+        except ValueError:
+            return number
+
+
+# Bool is represented as 'true' or 'false'
+def _str_to_boolean(val):
+    if not is_bool(val):
+        logger.warning(
+            "string passed for boolean conversion must be a boolean string true/false/yes/no"
+        )
+        return
+    if val.lower() in ["true", "yes"]:
+        return True
+    elif val.lower() in ["false", "no"]:
+        return False
+
+
+# Aerospike doesn't send float values
+# pretty print and other cpu stats can send float
+
+
+def _type_check_field_and_raw_values(section):
+    keys = []
+    # ip_regex = "[0-9]{1,2,3}(\.[0-9]{1,2,3})*"
+    for key in section:
+        if isinstance(section[key], dict):
+            _type_check_field_and_raw_values(section[key])
+        elif (
+            isinstance(section[key], list)
+            and len(section[key]) > 0
+            and isinstance(section[key][0], dict)
+        ):
+            for item in section[key]:
+                _type_check_field_and_raw_values(item)
+
+        else:
+            if (
+                isinstance(section[key], list)
+                or isinstance(section[key], int)
+                or isinstance(section[key], bool)
+                or isinstance(section[key], float)
+            ):
+                continue
+
+            if section[key] is None:
+                logger.debug("Value for key " + key + " is Null")
+                continue
+            # Some numbers have a.b.c.d* format, which matches with IP address
+            # So do a defensive check at starting.
+            # All type of address stats and config should be string so continue
+            # mesh-adderss, service-address.
+            if "addr" in key:
+                continue
+
+            # 3.9 config have ns name in some of the field names. {ns_name}-field_name
+            # Thease fields are already under ns section, so no need to put ns_name again.
+            # Remove ns name and put only filed name.
+            if re.match(r"\{.*\}-.*", key):
+                section[key.split("}-")[1]] = section.pop(key)
+
+            # Handle format like (a,b,c) this is a valid number
+            elif section[key].replace(",", "").isdigit():
+                number = _str_to_number(section[key].replace(",", ""))
+                if number < sys.maxsize:
+                    section[key] = number
+                else:
+                    keys.append(key)
+
+            # Handle format (a.b.cd.s), its valid number.
+            elif section[key].replace(".", "").isdigit():
+                number = _str_to_number(section[key].replace(".", ""))
+                if number < sys.maxsize:
+                    section[key] = number
+                else:
+                    keys.append(key)
+
+            # Handle bool
+            elif is_bool(section[key]):
+                section[key] = _str_to_boolean(section[key])
+
+            # Handle format (-ab,c,f)
+            elif section[key].lstrip("-").replace(",", "").isdigit():
+                num = section[key].lstrip("-").replace(",", "")
+                if num.isdigit():
+                    number = _str_to_number(num)
+                    section[key] = -1 * number
+
+    for key in keys:
+        section.pop(key, None)
