3ef807ea85670d6084359c45912041baba5243d7
Jesse Schmidt <jschmidt@aerospike.com>
2023-04-17 11:49:16 -0700

    fix: TOOLS-2484 `show latencies` aggregation issue

--- test/unit/view/test_templates.py
+++ test/unit/view/test_templates.py
@@ -2,6 +2,7 @@ import unittest
 from parameterized import parameterized
 
 from lib.view import templates
+from lib.view.sheet.decleration import EntryData
 
 
 class HelperTests(unittest.TestCase):
@@ -9,7 +10,39 @@ class HelperTests(unittest.TestCase):
         [
             ([0.5, 0.8, 0.3], [10, 15, 20], 0.511),
             ([0.5, 0.8, 0.3], [-1, 0, 1], 0),
+            ([4.0, 10.59, 8.35], [65701.6, 64926.1, 65567.2], 7.635),
         ],
     )
     def test_weighted_avg(self, values, weights, expected):
-        assert round(templates.weighted_avg(values, weights), 3) == expected
+        self.assertEqual(round(templates.weighted_avg(values, weights), 3), expected)
+
+    @parameterized.expand(
+        [
+            (
+                [
+                    EntryData(0.5, None, {"ops/sec": 10}, None, False, False),
+                    EntryData(0.8, None, {"ops/sec": 15}, None, False, False),
+                    EntryData(0.3, None, {"ops/sec": 20}, None, False, False),
+                ],
+                0.511,
+            ),
+        ],
+    )
+    def test_latency_weighted_avg(self, edatas, expected):
+        self.assertEqual(round(templates.latency_weighted_avg(edatas), 3), expected)
+
+    @parameterized.expand(
+        [
+            (
+                [
+                    EntryData(0.5, None, {"type": {"Total": 10}}, None, False, False),
+                    EntryData(0.8, None, {"type": {"Total": 15}}, None, False, False),
+                    EntryData(0.3, None, {"type": {"Total": 20}}, None, False, False),
+                ],
+                0.511,
+            ),
+        ],
+    )
+    def test_create_usage_weighted_avg(self, edatas, expected):
+        func = templates.create_usage_weighted_avg("type")
+        self.assertEqual(round(func(edatas), 3), expected)
