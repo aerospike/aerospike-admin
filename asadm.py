5a18244d208b5f0d872a3c796bbfb7ff3b90e776
Jesse S <jschmidt@aerospike.com>
2023-06-08 10:43:06 -0700

    fix: Don't catch SystemExit(0) an return exit code 130 (#166)
    * fix: Don't catch SystemExit(0) an return exit code 130

--- asadm.py
+++ asadm.py
@@ -900,7 +900,7 @@ def get_version():
 if __name__ == "__main__":
     try:
         asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
+    except KeyboardInterrupt:
         sys.exit(130)
     except Exception:
         pass
