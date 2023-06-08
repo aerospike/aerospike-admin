41165d24786b10bda79251ae364e8075f03e460d
Jesse Schmidt <jschmidt@aerospike.com>
2023-06-08 10:24:41 -0700

    fix tests

--- test/e2e/live_cluster/test_help.py
+++ test/e2e/live_cluster/test_help.py
@@ -3,7 +3,7 @@ from test.e2e import util
 
 
 class VersionTests(unittest.TestCase):
-    def test_return_code(self, cmd):
+    def test_return_code(self):
         args = "--help"
         o = util.run_asadm(args)
         self.assertEqual(o.returncode, 0)
