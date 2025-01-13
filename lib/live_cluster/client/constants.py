01100e812b01f9eea7b9c3b6b26456375654749b
Akash Chandra <achandra@aerospike.com>
2025-01-13 10:37:57 +0530

    fix: TOOLS-2986 manage roster command fix (#329)
    fixed the `manage roster` command that was broken due to error standardization in the server

--- lib/live_cluster/client/constants.py
+++ lib/live_cluster/client/constants.py
@@ -12,7 +12,7 @@ class ErrorsMsgs:
     DC_NODE_ADD_FAIL = "Failed to add node to XDR datacenter"
     DC_NODE_REMOVE_FAIL = "Failed to remove node from XDR datacenter"
     INVALID_REWIND = 'Invalid rewind. Must be int or "all"'
-    UNABLE_TO_DETERMINE_CLUSTER_STABILITY = "Failed to check cluster stability"
+    INFO_SERVER_ERROR_RESPONSE = 'Failed to execute info command - server error'
 
 
 DEFAULT_CONFIG_PATH = "/etc/aerospike/aerospike.conf"
