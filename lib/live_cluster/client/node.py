99fbb63326412f165ec6ed2bfa4717fa71bf21ae
Akash Chandra <achandra@aerospike.com>
2025-06-10 13:28:08 +0530

    fix: update info_sindex method to use 'sindex-list' for accurate data retrieval

--- lib/live_cluster/client/node.py
+++ lib/live_cluster/client/node.py
@@ -2542,7 +2542,7 @@ class Node(AsyncObject):
     async def info_sindex(self):
         return [
             client_util.info_to_dict(v, ":")
-            for v in client_util.info_to_list(await self._info("sindex"))
+            for v in client_util.info_to_list(await self._info("sindex-list"))
             if v != ""
         ]
 
