38c7ab345890e900cab97261d475a152ea85f025
grant <grant@iowntheinter.net>
2025-09-04 15:40:43 -0700

    debug scripts, and switch ubuntu 24.04 to installing pipenv from apt

--- .github/docker/install_deps.sh
+++ .github/docker/install_deps.sh
@@ -27,8 +27,7 @@ function install_deps_ubuntu22.04() {
 }
 
 function install_deps_ubuntu24.04() {
-  apt -y install ruby-rubygems make rpm git snapd curl binutils python3 python3-pip rsync
-  pip3 install pipenv
+  apt -y install ruby-rubygems make rpm git snapd curl binutils python3 python3-pip rsync pipenv
   gem install fpm
 }
 
