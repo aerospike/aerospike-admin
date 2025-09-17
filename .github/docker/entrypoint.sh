87160b4e63eec65cee8274c52961c69ec49c42fd
grant <grant@iowntheinter.net>
2025-09-17 18:37:23 +0000

    matrix debian13, redhat-el8, amazon-2023

--- .github/docker/entrypoint.sh
+++ .github/docker/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 env
-VERSION=$(git rev-parse HEAD | cut -c -8)
+
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+
+
 source $SCRIPT_DIR/build_package.sh
 
 if [ -d ".git" ]; then
@@ -14,13 +17,13 @@ fi
 
 
 function build_container() {
-  docker build -t asadmin-pkg-builder-"$1"-"$VERSION" -f .github/docker/Dockerfile-"$1" .
+  docker build --progress=plain -t asbackup-pkg-builder-"$1"-"$(git rev-parse HEAD | cut -c -8)" -f .github/docker/Dockerfile-"$1" .
 }
 
 
 function execute_build_image() {
   export BUILD_DISTRO="$1"
-  docker run -e BUILD_DISTRO -v $(realpath ../dist):/tmp/output asadmin-pkg-builder-"$BUILD_DISTRO"-"$VERSION"
+  docker run -e BUILD_DISTRO -v $(realpath ../dist):/tmp/output asbackup-pkg-builder-"$BUILD_DISTRO"-"$(git rev-parse HEAD | cut -c -8)"
   ls -laht ../dist
 }
 
@@ -69,12 +72,18 @@ elif grep -q 22.04 /etc/os-release; then
   ENV_DISTRO="ubuntu22.04"
 elif grep -q 24.04 /etc/os-release; then
   ENV_DISTRO="ubuntu24.04"
+elif grep -q "platform:el8" /etc/os-release; then
+  ENV_DISTRO="redhat-el8"
 elif grep -q "platform:el9" /etc/os-release; then
-  ENV_DISTRO="redhat-ubi9"
+  ENV_DISTRO="redhat-el9"
+elif grep -q "amazon_linux:2023" /etc/os-release; then
+  ENV_DISTRO="amazon-2023"
 elif grep -q "bullseye" /etc/os-release; then
   ENV_DISTRO="debian11"
 elif grep -q "bookworm" /etc/os-release; then
   ENV_DISTRO="debian12"
+elif grep -q "trixie" /etc/os-release; then
+  ENV_DISTRO="debian13"
 else
   cat /etc/os-release
   echo "os not supported"
@@ -91,15 +100,24 @@ if [ "$INSTALL" = "true" ]; then
   elif [ "$ENV_DISTRO" = "ubuntu24.04" ]; then
       echo "installing dependencies for Ubuntu 24.04"
       install_deps_ubuntu24.04
-  elif [ "$ENV_DISTRO" = "redhat-ubi9" ]; then
-      echo "installing dependencies for RedHat UBI9"
-      install_deps_redhat-ubi9
+  elif [ "$ENV_DISTRO" = "redhat-el8" ]; then
+      echo "installing dependencies for RedHat el8"
+      install_deps_redhat-el8
+  elif [ "$ENV_DISTRO" = "redhat-el9" ]; then
+      echo "installing dependencies for RedHat el9"
+      install_deps_redhat-el9
+  elif [ "$ENV_DISTRO" = "amazon-2023" ]; then
+      echo "installing dependencies for Amazon 2023"
+      install_deps_redhat-amazon-2023
   elif [ "$ENV_DISTRO" = "debian11" ]; then
       echo "installing dependencies for Debian 11"
       install_deps_debian11
   elif [ "$ENV_DISTRO" = "debian12" ]; then
       echo "installing dependencies for Debian 12"
       install_deps_debian12
+  elif [ "$ENV_DISTRO" = "debian13" ]; then
+      echo "installing dependencies for Debian 13"
+      install_deps_debian13
   else
       cat /etc/os-release
       echo "distro not supported"
@@ -110,10 +128,13 @@ elif [ "$BUILD_CONTAINERS" = "true" ]; then
   if  [ "$BUILD_DISTRO" = "all" ]; then
     build_container debian11
     build_container debian12
+    build_container debian13
     build_container ubuntu20.04
     build_container ubuntu22.04
     build_container ubuntu24.04
-    build_container redhat-ubi9
+    build_container redhat-el8
+    build_container redhat-el9
+    build_container amazon-2023
   else
     build_container $BUILD_DISTRO
   fi
@@ -125,14 +146,20 @@ if [ "$EXECUTE_BUILD" = "true" ]; then
         execute_build_image debian11
         echo "building package for Debian 12"
         execute_build_image debian12
+        echo "building package for Debian 13"
+        execute_build_image debian13
         echo "building package for Ubuntu 20.04"
         execute_build_image ubuntu20.04
         echo "building package for Ubuntu 22.04"
         execute_build_image ubuntu22.04
         echo "building package for Ubuntu 24.04"
         execute_build_image ubuntu24.04
-        echo "building package for RedHat UBI9"
-        execute_build_image redhat-ubi9
+        echo "building package for RedHat el8"
+        execute_build_image redhat-el8
+        echo "building package for RedHat el9"
+        execute_build_image redhat-el9
+        echo "building package for Amazon 2023"
+        execute_build_image amazon-2023
     else
         echo "building package for $BUILD_DISTRO"
         execute_build_image $BUILD_DISTRO
--- .github/docker/install_deps.sh
+++ .github/docker/install_deps.sh
@@ -45,6 +45,28 @@ function install_deps_debian12() {
   gem install fpm
 }
 
+function install_deps_debian13() {
+  apt -y install ruby-rubygems make rpm git snapd curl binutils python3 python3-pip rsync libssl3 libssl-dev lzma \
+                 lzma-dev libffi-dev
+  if [ "$(uname -m)" = "x86_64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  elif [ "$(uname -m)" = "aarch64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+  else
+      echo "unknown arch $(uname -m)"
+      exit 1
+  fi
+  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+  install /root/go/bin/asdf /usr/local/bin/asdf
+  asdf plugin add python https://github.com/asdf-community/asdf-python.git
+  asdf install python 3.10.18
+  asdf set python 3.10.18
+  asdf exec pip install --break-system-packages pipenv
+  gem install fpm
+}
+
 function install_deps_ubuntu20.04() {
   apt -y install ruby make rpm git snapd curl binutils python3 python3-pip rsync libssl1.1 libssl-dev \
                  lzma lzma-dev libffi-dev
@@ -111,8 +133,52 @@ function install_deps_ubuntu24.04() {
   asdf exec pip install --break-system-packages pipenv
   gem install fpm
 }
+function install_deps_redhat-el8() {
+  microdnf -y install ruby rpmdevtools make git python3 python3-pip rsync
+  if [ "$(uname -m)" = "x86_64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  elif [ "$(uname -m)" = "aarch64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+  else
+      echo "unknown arch $(uname -m)"
+      exit 1
+  fi
+  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+  install /root/go/bin/asdf /usr/local/bin/asdf
+  asdf plugin add python https://github.com/asdf-community/asdf-python.git
+  microdnf install -y gcc g++ make automake zlib zlib-devel libffi-devel openssl-devel bzip2-devel xz-devel xz xz-libs \
+                      sqlite sqlite-devel sqlite-libs
+  asdf install python 3.10.18
+  asdf set python 3.10.18
+  asdf exec pip install pipenv
+  gem install fpm
+}
 
-function install_deps_redhat-ubi9() {
+function install_deps_redhat-el9() {
+  microdnf -y install ruby rpmdevtools make git python3 python3-pip rsync
+  if [ "$(uname -m)" = "x86_64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  elif [ "$(uname -m)" = "aarch64" ]; then
+      curl -L https://go.dev/dl/go1.24.6.linux-arm64.tar.gz -o /tmp/go1.24.6.linux-arm64.tar.gz
+      mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-arm64.tar.gz -C /opt/golang
+  else
+      echo "unknown arch $(uname -m)"
+      exit 1
+  fi
+  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+  install /root/go/bin/asdf /usr/local/bin/asdf
+  asdf plugin add python https://github.com/asdf-community/asdf-python.git
+  microdnf install -y gcc g++ make automake zlib zlib-devel libffi-devel openssl-devel bzip2-devel xz-devel xz xz-libs \
+                      sqlite sqlite-devel sqlite-libs
+  asdf install python 3.10.18
+  asdf set python 3.10.18
+  asdf exec pip install pipenv
+  gem install fpm
+}
+function install_deps_redhat-amazon-2023() {
   microdnf -y install ruby rpmdevtools make git python3 python3-pip rsync
   if [ "$(uname -m)" = "x86_64" ]; then
       curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
