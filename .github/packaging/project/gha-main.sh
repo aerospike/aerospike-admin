df0774e98dd5920ddc8d4ced0c35176a06d75d14
pvinh-spike <pvinh@aerospike.com>
2025-12-12 08:10:41 +0000

    revert merge error.

--- .github/packaging/project/gha-main.sh
+++ .github/packaging/project/gha-main.sh
@@ -4,9 +4,7 @@ set -xeuo pipefail
 DISTRO="$1"
 env
 cd local
-if [ "$(git rev-parse --is-shallow-repository 2>/dev/null)" = "true" ]; then
-  git fetch --unshallow --tags --no-recurse-submodules
-fi
+git fetch --unshallow --tags --no-recurse-submodules
 git submodule update --init
 ls -laht
 git branch -v
