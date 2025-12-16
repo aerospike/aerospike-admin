55c6cc1bc17899a0adcba156f3858a4b19ab169d
pvinh-spike <pvinh@aerospike.com>
2025-12-16 01:29:10 +0000

    Initial commit.

--- .github/packaging/project/gha-main.sh
+++ .github/packaging/project/gha-main.sh
@@ -8,6 +8,15 @@ git fetch --unshallow --tags --no-recurse-submodules
 git submodule update --init
 ls -laht
 git branch -v
-.github/packaging/common/entrypoint.sh -c -d $DISTRO
-.github/packaging/common/entrypoint.sh -e -d $DISTRO
+
+if [ "${GITHUB_ACTIONS:-}" = "true" ] && [ "${USE_REMOTE_BUILDER_IMAGES:-false}" = "true" ]; then
+  # CI + remote images: don't build, just execute (will pull in execute_build_image)
+  .github/packaging/common/entrypoint.sh -e -d "$DISTRO"
+else
+  # Local or non-remote mode: build image then run it (existing behavior)
+  .github/packaging/common/entrypoint.sh -c -d "$DISTRO"
+  .github/packaging/common/entrypoint.sh -e -d "$DISTRO"
+fi
+
 ls -laht ../dist
+
--- .github/packaging/project/test/gha-test-main.sh
+++ .github/packaging/project/test/gha-test-main.sh
@@ -2,6 +2,7 @@
 set -xeuo pipefail
 DISTRO="$1"
 REPO_NAME="$2"
+PKG_VERSION="$3"
 env
 git fetch --unshallow --tags --no-recurse-submodules
-.github/packaging/common/example-test.sh "$DISTRO" "$REPO_NAME"
+.github/packaging/common/example-test.sh "$DISTRO" "$REPO_NAME" "$PKG_VERSION"
