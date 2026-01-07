863e13f1eb6e859cb23889b470cb86399a624c06
pvinh-spike <pvinh@aerospike.com>
2026-01-07 21:00:21 +0000

    fix: enable build & push builder images to jfrog

--- .github/packaging/project/gha-main.sh
+++ .github/packaging/project/gha-main.sh
@@ -9,14 +9,13 @@ git submodule update --init
 ls -laht
 git branch -v
 
-if [ "${GITHUB_ACTIONS:-}" = "true" ] && [ "${USE_REMOTE_BUILDER_IMAGES:-false}" = "true" ]; then
-  # CI + remote images: don't build, just execute (will pull in execute_build_image)
-  .github/packaging/common/entrypoint.sh -e -d "$DISTRO"
+if [ "${GITHUB_ACTIONS-}" = "true" ] && [ "${USE_REMOTE_BUILDER_IMAGES:-false}" = "true" ] && [ "${BUILD_BUILDER_IMAGES:-false}" = "false" ]; then
+	# CI + remote images: don't build, just execute (will pull in execute_build_image)
+	.github/packaging/common/entrypoint.sh -e -d "$DISTRO"
 else
-  # Local or non-remote mode: build image then run it (existing behavior)
-  .github/packaging/common/entrypoint.sh -c -d "$DISTRO"
-  .github/packaging/common/entrypoint.sh -e -d "$DISTRO"
+	# Local or non-remote mode: build image then run it (existing behavior)
+	.github/packaging/common/entrypoint.sh -c -d "$DISTRO"
+	.github/packaging/common/entrypoint.sh -e -d "$DISTRO"
 fi
 
 ls -laht ../dist
-
