f2330e3558465eabb4f9d9a59bc3d52c45869c31
pvinh-spike <pvinh@aerospike.com>
2026-01-10 04:21:34 +0000

    fix: remove USE_REMOTE_BUILDER_IMAGES, improve build_container.sh

--- .github/packaging/project/gha-main.sh
+++ .github/packaging/project/gha-main.sh
@@ -9,13 +9,7 @@ git submodule update --init
 ls -laht
 git branch -v
 
-if [ "${GITHUB_ACTIONS-}" = "true" ] && [ "${USE_REMOTE_BUILDER_IMAGES:-false}" = "true" ] && [ "${BUILD_BUILDER_IMAGES:-false}" = "false" ]; then
-	# CI + remote images: don't build, just execute (will pull in execute_build_image)
-	.github/packaging/common/entrypoint.sh -e -d "$DISTRO"
-else
-	# Local or non-remote mode: build image then run it (existing behavior)
-	.github/packaging/common/entrypoint.sh -c -d "$DISTRO"
-	.github/packaging/common/entrypoint.sh -e -d "$DISTRO"
-fi
+.github/packaging/common/entrypoint.sh -c -d "$DISTRO"
+.github/packaging/common/entrypoint.sh -e -d "$DISTRO"
 
 ls -laht ../dist
