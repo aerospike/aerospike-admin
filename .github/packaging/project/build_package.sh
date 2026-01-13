695b6e6ae8568d2b27aeeabf2e7f4db3b703fdb5
pvinh-spike <pvinh@aerospike.com>
2026-01-13 05:27:40 +0000

    fix: make sure env difined and export properly

--- .github/packaging/project/build_package.sh
+++ .github/packaging/project/build_package.sh
@@ -1,40 +1,40 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-function build_packages(){
-  if [ "$ENV_DISTRO" = "" ]; then
-    echo "ENV_DISTRO is not set"
-    return
-  fi
-  export PATH=$PATH:/opt/golang/go/bin
-  GIT_DIR=$(git rev-parse --show-toplevel)
+function build_packages() {
+	if [ "$ENV_DISTRO" = "" ]; then
+		echo "ENV_DISTRO is not set"
+		return
+	fi
+	export PATH=$PATH:/opt/golang/go/bin
+	GIT_DIR=$(git rev-parse --show-toplevel)
 
-  # build
-  cd "$GIT_DIR" || exit 1
-  make clean
-  make
+	# build
+	cd "$GIT_DIR" || exit 1
+	make clean
+	make
 
-  echo "build_package.sh version: $(git describe --tags --always --abbrev=7)"
-  VERSION=$(git describe --tags --always --abbrev=7)
-  export VERSION
+	echo "build_package.sh version: $(git describe --tags --always --abbrev=7)"
+	VERSION=${PKG_VERSION:-$(git describe --tags --always --abbrev=7)}
+	export VERSION
 
-  # package
-  cd "$GIT_DIR"/pkg || exit 1
-  make clean
-  echo "building package for $BUILD_DISTRO"
+	# package
+	cd "$GIT_DIR"/pkg || exit 1
+	make clean
+	echo "building package for $BUILD_DISTRO"
 
-  if [[ "$ENV_DISTRO" == *"ubuntu"* ]]; then
-    make deb
-  elif [[ "$ENV_DISTRO" == *"debian"* ]]; then
-    make deb
-  elif [[ "$ENV_DISTRO" == *"el"* ]]; then
-    make rpm
-  elif [[ "$ENV_DISTRO" == *"amzn"* ]]; then
-    make rpm
-  else
-    make tar
-  fi
+	if [[ $ENV_DISTRO == *"ubuntu"* ]]; then
+		make deb
+	elif [[ $ENV_DISTRO == *"debian"* ]]; then
+		make deb
+	elif [[ $ENV_DISTRO == *"el"* ]]; then
+		make rpm
+	elif [[ $ENV_DISTRO == *"amzn"* ]]; then
+		make rpm
+	else
+		make tar
+	fi
 
-  mkdir -p /tmp/output/"$ENV_DISTRO"
-  cp -a "$GIT_DIR"/pkg/target/* /tmp/output/"$ENV_DISTRO"
+	mkdir -p /tmp/output/"$ENV_DISTRO"
+	cp -a "$GIT_DIR"/pkg/target/* /tmp/output/"$ENV_DISTRO"
 }
