3a01da07539c49eec698782500b1b5b7b1472eed
pvinh-spike <pvinh@aerospike.com>
2026-01-13 06:30:51 +0000

    fix: no git fetch

--- .github/packaging/project/gha-main.sh
+++ .github/packaging/project/gha-main.sh
@@ -4,12 +4,13 @@ set -xeuo pipefail
 DISTRO="$1"
 env
 cd local
-if [ "$(git rev-parse --is-shallow-repository 2>/dev/null)" = "true" ]; then
-	git fetch --unshallow --tags --no-recurse-submodules
-fi
-git submodule update --init
+#if [ "$(git rev-parse --is-shallow-repository 2>/dev/null)" = "true" ]; then
+#git fetch --unshallow --tags --no-recurse-submodules
+#fi
+#git submodule update --init
 ls -laht
 git branch -v
+git describe
 
 .github/packaging/common/entrypoint.sh -c -d "$DISTRO"
 .github/packaging/common/entrypoint.sh -e -d "$DISTRO"
