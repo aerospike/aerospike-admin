458ecdd657a86fcc9a71f67fc9a49935ccb882bf
pvinh-spike <pvinh@aerospike.com>
2026-01-13 07:25:07 +0000

    fix: remove bakeed source inside container, mount source at run time, force build new images

--- .github/packaging/project/gha-main.sh
+++ .github/packaging/project/gha-main.sh
@@ -4,13 +4,10 @@ set -xeuo pipefail
 DISTRO="$1"
 env
 cd local
-#if [ "$(git rev-parse --is-shallow-repository 2>/dev/null)" = "true" ]; then
-#git fetch --unshallow --tags --no-recurse-submodules
-#fi
-#git submodule update --init
+git fetch --unshallow --tags --no-recurse-submodules
+git submodule update --init
 ls -laht
 git branch -v
-git describe
 
 .github/packaging/common/entrypoint.sh -c -d "$DISTRO"
 .github/packaging/common/entrypoint.sh -e -d "$DISTRO"
