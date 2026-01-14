cd5ba3ec7250a2dc7ba2ac6bd344da35589a4e32
pvinh-spike <pvinh@aerospike.com>
2026-01-14 06:22:20 +0000

    chore: Coalesce sign, deploy jobs, add create bundle, enable prebuilt runner

--- .github/packaging/project/test/gha-test-main.sh
+++ .github/packaging/project/test/gha-test-main.sh
@@ -2,6 +2,8 @@
 set -xeuo pipefail
 DISTRO="$1"
 REPO_NAME="$2"
+PKG_VERSION="$3"
+PACKAGE_NAME="$4"
 env
 git fetch --unshallow --tags --no-recurse-submodules
-.github/packaging/common/example-test.sh "$DISTRO" "$REPO_NAME"
+.github/packaging/common/example-test.sh "$DISTRO" "$REPO_NAME" "$PKG_VERSION" "$PACKAGE_NAME"
