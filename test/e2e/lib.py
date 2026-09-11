aeec5170e45204b7c5b92c21269fc595cad669ec
Akash <achandra@aerospike.com>
2026-09-11 15:18:18 -0700

    fix: update SERVER_TAG

--- test/e2e/lib.py
+++ test/e2e/lib.py
@@ -52,7 +52,7 @@ IMAGE_REPO = os.environ.get(
     "ASADM_E2E_IMAGE_REPO",
     "artifact.aerospike.io/database-docker-test-local/aerospike-server-enterprise",
 )
-SERVER_TAG = os.environ.get("ASADM_E2E_SERVER_TAG", "8.2.0.0-20260908214755")
+SERVER_TAG = os.environ.get("ASADM_E2E_SERVER_TAG", "8.2.0.0_20260911T220300Z")
 
 WORK_DIRECTORY = "work"
 LUA_DIRECTORY = "work/lua"
